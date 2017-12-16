7fb8090a7da2f6b450017b445b18c7d7d99dd63f
Aleksandr <a@test.com>
2017-12-17 00:54:04 +0200

    Тестирование вариантов

--- www/backuper/classes/Backuper.php
+++ www/backuper/classes/Backuper.php
@@ -2,7 +2,10 @@
 
 class Backuper{
 
+    protected $root;
+
     public function __construct(){
+        $this->root = $_SERVER['DOCUMENT_ROOT'];
     }
 
     public static function Factory($config) {
@@ -11,10 +14,34 @@ class Backuper{
     }
 
     public function makeBackupFiles() {
-
+        //$phar = new PharData('project.tar');
+        //$res = $phar->buildFromDirectory($this->root . '/site');
+       // debug($res);
+//        $tmpDir = sys_get_temp_dir();
+//        $path = './';
+//        // build command
+//        $outputFile = $tmpDir . DIRECTORY_SEPARATOR . 'test' . '.tar';
+//        $cmd = strtr('@tar -cf @output -C ./ @file', array(
+//            '@tar' => 'tar',
+//            '@output' => $outputFile,
+//            '@dir' => dirname($path),
+//            '@file' => ''//basename($path),
+//        ));
+//        // exclude files
+//        $option['exclude'] = 'tar';
+//        if (!empty($option['exclude'])) {
+//            foreach (explode(',', $option['exclude']) as $file) {
+//                $cmd .= strtr(' --exclude="@file"', array(
+//                    '@file' => trim($file)
+//                ));
+//            }
+//        }
+//        var_dump($cmd);
+//        shell_exec($cmd);
     }
 
 }
+//https://github.com/AlexeyFreelancer/BackupTask
 // example
 function backupFiles($backup_folder, $backup_name, $dir)
 {
--- www/index.php
+++ www/index.php
@@ -1,2 +1,3 @@
 <?php
 echo 'Backup test';
+var_dump(sys_get_temp_dir());
