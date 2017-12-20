7603bbc46838d5cdb494b0ae71f6aaa22563d12a
Александр Корниенко <kornsasha89@gmail.com>
2017-12-20 11:26:04 +0200

    Добавлены библиотеки длфя создания архива

--- www/backuper/run.php
+++ www/backuper/run.php
@@ -3,8 +3,17 @@
 require_once '../functions.php';
 // подключение классов
 require_once 'classes/Backuper.php';
+require_once 'classes/Tar.php';
 
 $config = require_once 'config.php';
 
-$backuper = Backuper::Factory($config);
-$backuper->makeBackupFiles();
+//$backuper = Backuper::Factory($config);
+//$backuper->makeBackupFiles();
+$output = shell_exec('ls');
+echo "<pre>$output</pre>";
+//shell_exec("tar -zcf ./archive_name.tar.gz ./");
+var_dump(class_exists('PEAR'));
+$tar = new Archive_Tar('test.tar');
+$tar->_debug = true;
+$tar->add(['../site']);
+var_dump($tar);
